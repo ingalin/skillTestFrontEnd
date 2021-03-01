d4dd1ff44e5aa89c9511146c272dae4f76f8c215
ingalin <ingalin@inbox.lv>
2021-03-01 10:50:10 -0500

    navigation added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
 $(document).ready(function () {
-    // < !--Init Skitter-- >
+
+    // Start page by going to the top - to keep colors correctly
+    $(document).scrollTop(0);
+
+    // Init Skitter (slider)
     $('.skitter-large').skitter({
         responsive: {
             small: {
@@ -15,4 +18,23 @@ $(document).ready(function () {
             }
         }
     });
+
+    // Change color menu, check if it's scorlled, if it is, change color
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > $("header").height()) {
+            $(".headerMenuDrop ul.mobileSubMenu").removeClass("colorChange1").addClass("colorChange2");
+            $(".menuButton").addClass("buttonColor2");
+        }
+        else {
+            $(".headerMenuDrop ul.mobileSubMenu").removeClass("colorChange2").addClass("colorChange1");
+            $(".menuButton").removeClass("buttonColor2").addClass("buttonColor1");
+        }
+    });
+
+
+
+
+
+
+
 });
